11a5ae97a07d132e9353b3a2db8070854af6fe0c
Giovani Vercauteren <4256757+iBelg@users.noreply.github.com>
2026-02-01 12:07:08 +0100

    First test deploy

---
+++ app/lib/data.ts
@@ -0,0 +1,15 @@
+import { Skill } from "./definitions";
+
+export const skills: Skill[]  = [
+    { name: 'JavaScript', level: 100, type: 'language' },
+    { name: 'TypeScript', level: 100, type: 'language' },
+    { name: 'React', level: 50, type: 'framework' },
+    { name: 'Next.js', level: 50, type: 'framework' },
+    { name: 'Node.js', level: 80, type: 'runtime' },
+    { name: 'Vue.js', level: 95, type: 'framework' },
+    { name: 'HTML', level: 100, type: 'language' },
+    { name: 'CSS', level: 100, type: 'language' },
+    { name: 'Git', level: 90, type: 'tool' },
+    { name: 'Docker', level: 50, type: 'tool' },
+    { name: 'SQL', level: 70, type: 'language' },
+];
