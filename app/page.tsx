60e04ec87634c24b317aa5a27c74313e9ee28ee4
Giovani Vercauteren <4256757+iBelg@users.noreply.github.com>
2026-02-01 18:52:30 +0100

    update resume and title

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <div className="lg:flex lg:gap-8">
       <header className="lg:sticky lg:top-0 lg:max-h-screen lg:w-[40%] lg:flex lg:flex-col lg:justify-between lg:py-12">
-        <TitleName name="Giovani Vercauteren" title="Software Developer" description="Versatile and passionate about creating impactful web applications" />
+        <TitleName name="Giovani Vercauteren" title="B.S. Computer Science" description="Passionate about software development and technology" />
         <ContactLinks />
       </header>
       <main className=" lg:w-[60%] lg:py-12 pt-16">
