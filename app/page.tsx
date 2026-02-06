8679e6f21feca3dafb7153be373c8c67e7055c26
Giovani Vercauteren <4256757+iBelg@users.noreply.github.com>
2026-02-06 09:51:43 +0100

    fix: title

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <div className="lg:flex lg:gap-8">
       <header className="lg:sticky lg:top-0 lg:max-h-screen lg:w-[40%] lg:flex lg:flex-col lg:justify-between lg:py-12">
-        <TitleName name="Giovani Vercauteren" title="B.S. Computer Science" description="Passionate about software development and technology" />
+        <TitleName name="Giovani Vercauteren" title="Bachelor Computer Science" description="Passionate about software development and technology" />
         <ContactLinks />
       </header>
       <main className=" lg:w-[60%] lg:py-12 pt-16">
